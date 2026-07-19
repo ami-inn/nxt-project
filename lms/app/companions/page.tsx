042be52ced7208e778fd2772a98a63ebc58132fa
ami-inn <amipk2001@gmail.com>
2026-07-18 23:26:15 -0700

    search component added

--- lms/app/companions/page.tsx
+++ lms/app/companions/page.tsx
@@ -1,6 +1,17 @@
+import { getAllCompanions } from '@/lib/actions/companion.action';
 import React from 'react'
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({searchParams}:SearchParams) => {
+  
+ const filters = await searchParams;
+    const subject = filters.subject ? filters.subject : '';
+    const topic = filters.topic ? filters.topic : '';
+
+    const companions = await getAllCompanions({ subject, topic });
+
+    console.log(companions)
+
+  
   return (
     <div>
       
