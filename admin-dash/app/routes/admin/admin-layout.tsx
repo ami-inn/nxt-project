fbb3b0acb54748740489cfbda025e9413c9597eb
ami-inn <amipk2001@gmail.com>
2026-07-24 06:24:09 -0700

    update: modified the sidebar and layout

--- admin-dash/app/routes/admin/admin-layout.tsx
+++ admin-dash/app/routes/admin/admin-layout.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import  { Outlet } from "react-router";
+import ej2Navigations from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "../../../components";
+
+const { SidebarComponent } = ej2Navigations;
 
 const AdminLayout = () => {
   return (
-    <div className="admin-layout">
-      Mobile sidebar
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
-      <aside className="children">
-        <Outlet />
-      </aside>
-    </div>
+      <div className="admin-layout">
+            {/* <MobileSidebar /> */}
+
+            <aside className="w-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
+
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
   );
 };
 
