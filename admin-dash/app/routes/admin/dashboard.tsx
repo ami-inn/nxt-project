8990de1a45edcd8541c1763a248b51062d05e58a
ami-inn <amipk2001@gmail.com>
2026-07-24 06:39:49 -0700

    udpate dash

--- admin-dash/app/routes/admin/dashboard.tsx
+++ admin-dash/app/routes/admin/dashboard.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div className='text-blue-500'>
-      this is dashboard 
+      this is dashboard  view
+      
     </div>
   )
 }
