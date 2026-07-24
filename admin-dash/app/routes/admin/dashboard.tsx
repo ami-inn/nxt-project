99067a30e3dbb115d6399a9fb89e5965e3287be2
ami-inn <amipk2001@gmail.com>
2026-07-24 07:05:41 -0700

    update: stats card implemented on dashboard

--- admin-dash/app/routes/admin/dashboard.tsx
+++ admin-dash/app/routes/admin/dashboard.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import Header from '../../../components/Header'
+import React from "react";
+import Header from "../../../components/Header";
+import StatsCard from "../../../components/StatsCard";
 
 const Dashboard = () => {
-    const user = {name: 'John Doe'}; // Replace with actual user data from loader or context
-  return (
-    <main className='dashboard wrapper'>
-        <Header
-                title={`Welcome ${user?.name ?? 'Guest'} 👋`}
-                description="Track activity, trends and popular destinations in real time"
-            />
-    </main>
-  )
-}
+  const user = { name: "John Doe" }; // Replace with actual user data from loader or context
 
-export default Dashboard
- 
+  const dashboardStats = {
+    totalUsers: 1200,
+    userJoined: { currentMonth: 50, previousMonth: 40 },
+    totalTrips: 300,
+    tripsCreated: { currentMonth: 20, previousMonth: 15 },
+  };
+
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            currentMonthCount={dashboardStats.userJoined.currentMonth}
+            lastMonthCount={dashboardStats.userJoined.previousMonth}
+            headerTitle="New Users"
+            total={dashboardStats.totalUsers}
+          />
+            <StatsCard
+            currentMonthCount={dashboardStats.userJoined.currentMonth}
+            lastMonthCount={dashboardStats.userJoined.previousMonth}
+            headerTitle="New Users"
+            total={dashboardStats.totalUsers}
+          />
+            <StatsCard
+            currentMonthCount={dashboardStats.userJoined.currentMonth}
+            lastMonthCount={dashboardStats.userJoined.previousMonth}
+            headerTitle="New Users"
+            total={dashboardStats.totalUsers}
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Dashboard;
